a3bc16773e6dc0f61d8fbc1cb4af2b49f8f654fa
Kostya <kostyashtc@gmail.com>
2020-03-08 14:36:02 +0300

    final_1

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,19 @@ import java.io.IOException;
 
 public class Main {
 
+	static {
+		try {
+			System.load("C:\\Program Files\\Java\\jdk1.8.0_181\\jre\\bin\\deploy.dll");
+		} catch (UnsatisfiedLinkError e) {
+			System.err.println("Native code library failed to load\n");
+			System.exit(1);
+		}
+	}
+
 	public static void main(String[] args) throws IOException {
 
 		// Scanner sc = new Scanner(System.in);
-		Run run = new Run(new File("-f D:\\java\\javaEE\\projects\\TaskIBA\\keys.txt".substring(3)),
-				"-cmd whoami".substring(5),
-				"-rk HKEY_USERS\\.DEFAULT\\Control Panel\\Accessibility\\HighContrast\\Flags".substring(4));
+		Run run = new Run(new File(args[0].substring(3)), args[1].substring(5), args[2].substring(4));
 		run.readFile();
 		run.runCommand();
 		run.runWithKeyReester();
