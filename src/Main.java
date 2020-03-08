e006f1e573e185b25e01f372bccc2b0f03c3b9c0
Kostya <kostyashtc@gmail.com>
2020-03-08 19:38:29 +0300

    final project

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.io.IOException;
 
 public class Main {
-
+	
 	static {
 		try {
 			System.load("C:\\Program Files\\Java\\jdk1.8.0_181\\jre\\bin\\deploy.dll");
@@ -10,12 +10,14 @@ public class Main {
 			System.err.println("Native code library failed to load\n");
 			System.exit(1);
 		}
+
 	}
 
 	public static void main(String[] args) throws IOException {
 
-		// Scanner sc = new Scanner(System.in);
-		Run run = new Run(new File(args[0].substring(3)), args[1].substring(5), args[2].substring(4));
+		Run run = new Run(new File("-f D:\\java\\javaEE\\projects\\TaskIBA\\keys.txt".substring(3)),
+				"-cmd whoami".substring(5),
+				"-rk HKEY_LOCAL_MACHINE\\SOFTWARE\\JavaSoft\\Java Development Kit\\1.8.0_181\\MSI\\INSTALLDIR".substring(4));
 		run.readFile();
 		run.runCommand();
 		run.runWithKeyReester();
@@ -25,5 +27,5 @@ public class Main {
 }
 
 // args[0] = "-f D:\\java\\javaEE\\projects\\TaskIBA\\keys.txt"
-// args[1] = "-cmd command";
-// args[2] = "-rk SOFTWARE\\JavaSoft\\Java Development Kit\\1.8.0_181\\MSI";
+// args[1] = "-cmd whoami";
+// args[2] = "-rk HKEY_LOCAL_MACHINE\SOFTWARE\JavaSoft\Java Development Kit\1.8.0_181\MSI\INASTALLDIR";
